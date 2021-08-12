5c56bd39483d2dc3a77421ac1c9e6a5642caf61a
ngocthinhit <88842630+ngocthinhit@users.noreply.github.com>
2021-08-12 21:59:13 +0700

    init
    init

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào cậu!',
+    introDesc: `Ơ kìa nhìn cái rì thế ?`,
+    btnIntro: 'Nhấn đây nè',
+    title: 'Hôm nay nắng quá nhỉ ',
+    desc: 'Cậu có muốn làm ny tớ không?',
+    btnYes: 'Có ạ',
+    btnNo: 'Mơ đi cưng',
+    question:'Đồng ý nhanh thế á? Tại sao lại nhấn có???',
+    btnReply: 'Trả lời ở đây',
+    reply: 'Yêu thì yêu không yêu thì yêu, chịu thì chịu không chịu thì chịu <33333333',
+    mess: 'Cảm ơn nè <3',
+    messDesc: 'Im just kidding...',
+    btnAccept: 'Vâng <3',
+    messLink: 'http://www.facebook.com/messages/t/100006317044070' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
